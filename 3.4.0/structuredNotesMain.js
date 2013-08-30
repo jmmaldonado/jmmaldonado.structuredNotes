b50ca8f564359b9402bdcde2b6a56c9a6918f8e0
Nimer <jmmaldonado@gmail.com>
2013-08-30 20:16:07 +0200

    wysihtml5 funcionando
    Ya envia los cambios al div que lo llamo

--- 3.4.0/structuredNotesMain.js
+++ 3.4.0/structuredNotesMain.js
@@ -9,14 +9,16 @@ $(document).ready(function() {
     
         $("#pageOpciones").bind("pageshow", loadSettings);
         
-        //Abrir pagina de edicion de campo al hacer doble click en las areas correspondientes de la minuta
-        $("#tablaMinuta_Asistentes").dblclick(abrirDialogoEdicion);
-        $("#tablaMinuta_Acciones").dblclick(abrirDialogoEdicion);
-        $("#tablaMinuta_Notas").dblclick(abrirDialogoEdicion);
-        $("#tablaMinuta_S").dblclick(abrirDialogoEdicion);
-        $("#tablaMinuta_W").dblclick(abrirDialogoEdicion);
-        $("#tablaMinuta_O").dblclick(abrirDialogoEdicion);
-        $("#tablaMinuta_T").dblclick(abrirDialogoEdicion);
+        //Abrir pagina de edicion de campo al hacer click en las areas correspondientes de la minuta
+        $("#tablaMinuta_Asistentes").click(abrirDialogoEdicion);
+        $("#tablaMinuta_Acciones").click(abrirDialogoEdicion);
+        $("#tablaMinuta_Notas").click(abrirDialogoEdicion);
+        $("#tablaMinuta_S").click(abrirDialogoEdicion);
+        $("#tablaMinuta_W").click(abrirDialogoEdicion);
+        $("#tablaMinuta_O").click(abrirDialogoEdicion);
+        $("#tablaMinuta_T").click(abrirDialogoEdicion);
+        
+        $("#pageEdicionMinutaTables").bind("pageshow", prepararPaginaEdicionMinuta);
         
         
     
@@ -24,64 +26,8 @@ $(document).ready(function() {
     
     //Boton de pruebas
     $('#botPruebas').click(function() {
-        console.log("[INFO] Asignando tamaños a los bloques de la minuta");
-        console.time("tamanoBloques");
+        console.log("[INFO] BOTON DE PRUEBAS");
         
-        var realStateVerticalDisponible = obtenerRealStateVerticalDisponible();
-        var alturaAsistentesAcciones = Math.floor(realStateVerticalDisponible * localStorage.alturaAsistentesAcciones / 100);
-        var alturaSW = Math.floor(realStateVerticalDisponible * localStorage.alturaSWOT / 100);
-        var alturaOT = alturaSW; //Las filas SW y OT deberan ser del mismo tamaño
-        var alturaNotas = realStateVerticalDisponible - alturaAsistentesAcciones - alturaSW - alturaOT;
-        
-        console.log("[DEBUG] alturaAsistentesAcciones = " + alturaAsistentesAcciones);
-        console.log("[DEBUG] alturaSW = " + alturaSW);
-        console.log("[DEBUG] alturaOT = " + alturaOT);
-        console.log("[DEBUG] alturaNotas = " + alturaNotas);
-        
-        
-        $("#tablaMinuta_Asistentes").css("height", alturaAsistentesAcciones + "px");
-        $("#tablaMinuta_Asistentes").css("max-height", alturaAsistentesAcciones + "px");
-        
-        $("#tablaMinuta_Acciones").css("height", alturaAsistentesAcciones + "px");
-        $("#tablaMinuta_Acciones").css("max-height", alturaAsistentesAcciones + "px");
-            
-        $("#tablaMinuta_Notas").css("height", alturaNotas + "px");
-        $("#tablaMinuta_Notas").css("max-height", alturaNotas + "px");
-        
-        
-        $("#tablaMinuta_S").css("height", alturaSW + "px");
-        $("#tablaMinuta_S").css("max-height", alturaSW + "px");
-        
-        $("#tablaMinuta_W").css("height", alturaSW + "px");
-        $("#tablaMinuta_W").css("max-height", alturaSW + "px");
-        
-        $("#tablaMinuta_O").css("height", alturaOT + "px");
-        $("#tablaMinuta_O").css("max-height", alturaOT + "px");
-        
-        $("#tablaMinuta_T").css("height", alturaOT + "px");
-        $("#tablaMinuta_T").css("max-height", alturaOT + "px");
-        
-        if (typeof scrollAsistentes != 'undefined') scrollAsistentes.destroy();
-        if (typeof scrollAcciones != 'undefined') scrollAcciones.destroy();
-        if (typeof scrollNotas != 'undefined') scrollNotas.destroy();
-        if (typeof scrollS != 'undefined') scrollS.destroy();
-        if (typeof scrollW != 'undefined') scrollW.destroy();
-        if (typeof scrollO != 'undefined') scrollO.destroy();
-        if (typeof scrollT != 'undefined') scrollT.destroy();
-        
-        scrollAsistentes = new iScroll('tablaMinuta_Asistentes');
-        scrollAcciones = new iScroll('tablaMinuta_Acciones');
-        scrollNotas = new iScroll('tablaMinuta_Notas');
-        scrollS = new iScroll('tablaMinuta_S');
-        scrollW = new iScroll('tablaMinuta_W');
-        scrollO = new iScroll('tablaMinuta_O');
-        scrollT = new iScroll('tablaMinuta_T');
-        
-        
-        
-        document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
-        
-        console.timeEnd("tamanoBloques");
     });
     
     
@@ -119,15 +65,17 @@ function abrirDialogoEdicion() {
     //Mostramos en el titulo del dialogo el campo correspondiente al texto en edicion
     $("#spanTituloEdicionCampo").html(campo);
     
+    //Solo inicializamos el WYSIHTML5 una vez. Luego lo metemos como variable global (window. ...)
+    if(!window.editorWYSIHTML5) {
+        //Configuramos el text area como un editor rico: wysihtml5
+        window.editorWYSIHTML5 = new wysihtml5.Editor("wysihtml5-textarea", { // id of textarea element
+            toolbar:      "wysihtml5-toolbar", // id of toolbar element
+            parserRules:  wysihtml5ParserRules // defined in parser rules set 
+        });
+    }
+    
     //Mostramos en el textArea el html original del campo para editar
-    $("#wysihtml5-textarea").html($this.html());
-    
-    
-    //Configuramos el text area como un editor rico: wysihtml5
-    var editor = new wysihtml5.Editor("wysihtml5-textarea", { // id of textarea element
-        toolbar:      "wysihtml5-toolbar", // id of toolbar element
-        parserRules:  wysihtml5ParserRules // defined in parser rules set 
-    });
+    window.editorWYSIHTML5.setValue($this.html());
     
     //Asignacion Event Handlers de los botones
     $("#botGuardarEdicionCampo").click(guardarCampoEditado);
@@ -138,7 +86,7 @@ function abrirDialogoEdicion() {
 
 
 
-/* ToDo: Destroy wysihtml5 textarea antes de cerrar */
+
 function guardarCampoEditado() {
     console.log("[DEBUG] Entrando en guardarCampoEditado fichero structuredNotesMain.js");
     
@@ -155,4 +103,67 @@ function guardarCampoEditado() {
     $.mobile.changePage( "#pageEdicionMinutaTables");
     
     console.log("[DEBUG] Saliendo de guardarCampoEditado fichero structuredNotesMain.js");
+}
+
+
+
+
+
+function prepararPaginaEdicionMinuta() {
+
+    console.log("[DEBUG] Entrando en prepararPaginaEdicionMinuta fichero structuredNotesMain.js");
+    //console.time("tamanoBloques");
+    
+    var realStateVerticalDisponible = obtenerRealStateVerticalDisponible();
+    var alturaAsistentesAcciones = Math.floor(realStateVerticalDisponible * localStorage.alturaAsistentesAcciones / 100);
+    var alturaSW = Math.floor(realStateVerticalDisponible * localStorage.alturaSWOT / 100);
+    var alturaOT = alturaSW; //Las filas SW y OT deberan ser del mismo tamaño
+    var alturaNotas = realStateVerticalDisponible - alturaAsistentesAcciones - alturaSW - alturaOT;
+    
+    console.log("[DEBUG] alturaAsistentesAcciones = " + alturaAsistentesAcciones);
+    console.log("[DEBUG] alturaSW = " + alturaSW);
+    console.log("[DEBUG] alturaOT = " + alturaOT);
+    console.log("[DEBUG] alturaNotas = " + alturaNotas);
+    
+    
+    $("#tablaMinuta_Asistentes").css("height", alturaAsistentesAcciones + "px");
+    $("#tablaMinuta_Asistentes").css("max-height", alturaAsistentesAcciones + "px");
+    
+    $("#tablaMinuta_Acciones").css("height", alturaAsistentesAcciones + "px");
+    $("#tablaMinuta_Acciones").css("max-height", alturaAsistentesAcciones + "px");
+        
+    $("#tablaMinuta_Notas").css("height", alturaNotas + "px");
+    $("#tablaMinuta_Notas").css("max-height", alturaNotas + "px");
+    
+    
+    $("#tablaMinuta_S").css("height", alturaSW + "px");
+    $("#tablaMinuta_S").css("max-height", alturaSW + "px");
+    
+    $("#tablaMinuta_W").css("height", alturaSW + "px");
+    $("#tablaMinuta_W").css("max-height", alturaSW + "px");
+    
+    $("#tablaMinuta_O").css("height", alturaOT + "px");
+    $("#tablaMinuta_O").css("max-height", alturaOT + "px");
+    
+    $("#tablaMinuta_T").css("height", alturaOT + "px");
+    $("#tablaMinuta_T").css("max-height", alturaOT + "px");
+    
+    if (typeof scrollAsistentes != 'undefined') scrollAsistentes.destroy();
+    if (typeof scrollAcciones != 'undefined') scrollAcciones.destroy();
+    if (typeof scrollNotas != 'undefined') scrollNotas.destroy();
+    if (typeof scrollS != 'undefined') scrollS.destroy();
+    if (typeof scrollW != 'undefined') scrollW.destroy();
+    if (typeof scrollO != 'undefined') scrollO.destroy();
+    if (typeof scrollT != 'undefined') scrollT.destroy();
+    
+    scrollAsistentes = new iScroll('tablaMinuta_Asistentes');
+    scrollAcciones = new iScroll('tablaMinuta_Acciones');
+    scrollNotas = new iScroll('tablaMinuta_Notas');
+    scrollS = new iScroll('tablaMinuta_S');
+    scrollW = new iScroll('tablaMinuta_W');
+    scrollO = new iScroll('tablaMinuta_O');
+    scrollT = new iScroll('tablaMinuta_T');
+    
+    
+    console.log("[DEBUG] Saliendo de prepararPaginaEdicionMinuta fichero structuredNotesMain.js");
 }
